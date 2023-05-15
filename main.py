968aaca4eac10278988c8cf37e2a7db2f7e579d3
Moein <pistolparody@gmail.com>
2023-05-15 19:22:50 +0330

    added edge rectangles

--- main.py
+++ main.py
@@ -12,9 +12,24 @@ cr.screen = pg.display.set_mode(
 )
 
 cr.event_holder = EventHolder()
+cr.event_holder.should_render_debug = True
+
+rect = Rect(0, 0, 15, 15)
+edges = [rect.copy() for i in range(4)]
+
+edges[1].x = cr.screen.get_width() - edges[1].w
+edges[2].x = cr.screen.get_width() - edges[2].w
+edges[2].y = cr.screen.get_height() - edges[2].h
+edges[3].y = cr.screen.get_height() - edges[3].h
 
 
 while not cr.event_holder.should_quit:
-    cr.screen.fill("white")
+    cr.screen.fill("gray")
     cr.event_holder.get_events()
+
+
+    if cr.event_holder.should_render_debug:
+        for rect in edges:
+            pg.draw.rect(cr.screen,"black",rect,width=2)
+
     pg.display.update()
